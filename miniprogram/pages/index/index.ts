a534dae0ef757431ad497970ffcef11a26ffd9a1
leeing <372711472@qq.com>
2024-09-09 15:35:24 +0800

    提升率计算

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -5,49 +5,105 @@ const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia0
 
 Component({
   data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    bjl: 100,
+    bjsh: 200,
+    sq: 300,
+    shts: 0,
+    jnsh: 0,
+    boss: 0,
+    kzyc: 0,
+
+    bjlAfter: 100,
+    bjshAfter: 200,
+    sqAfter: 300,
+    shtsAfter: 0,
+    jnshAfter: 0,
+    bossAfter: 0,
+    kzycAfter: 0,
+
+    res: '',
+  },
+  attached() {
+    const _bjl = wx.getStorageSync('bjl')
+    const _bjsh = wx.getStorageSync('bjsh')
+    const _sq = wx.getStorageSync('sq')
+    const _shts = wx.getStorageSync('shts')
+    const _jnsh = wx.getStorageSync('jnsh')
+    const _boss = wx.getStorageSync('boss')
+    const _kzyc = wx.getStorageSync('kzyc')
+
+    const params: any = {}
+
+    if (_bjl) {
+      params.bjl = Number(_bjl)
+    }
+    if (_bjsh) {
+      params.bjsh = Number(_bjsh)
+    }
+    if (_sq) {
+      params.sq = Number(_sq)
+    }
+    if (_shts) {
+      params.shts = Number(_shts)
+    }
+    if (_jnsh) {
+      params.jnsh = Number(_jnsh)
+    }
+    if (_boss) {
+      params.boss = Number(_boss)
+    }
+    if (_kzyc) {
+      params.kzyc = Number(_kzyc)
+    }
+    this.setData(params)
   },
   methods: {
-    // 事件处理函数
-    bindViewTap() {
-      wx.navigateTo({
-        url: '../logs/logs',
+    bindTopdown() {
+      this.setData({
+        bjlAfter: this.data.bjl,
+        bjshAfter: this.data.bjsh,
+        sqAfter: this.data.sq,
+        shtsAfter: this.data.shts,
+        jnshAfter: this.data.jnsh,
+        bossAfter: this.data.boss,
+        kzycAfter: this.data.kzyc,
       })
     },
-    onChooseAvatar(e: any) {
-      const { avatarUrl } = e.detail
-      const { nickName } = this.data.userInfo
+    bindBottomup() {
       this.setData({
-        "userInfo.avatarUrl": avatarUrl,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+        bjl: this.data.bjlAfter,
+        bjsh: this.data.bjshAfter,
+        sq: this.data.sqAfter,
+        shts: this.data.shtsAfter,
+        jnsh: this.data.jnshAfter,
+        boss: this.data.bossAfter,
+        kzyc: this.data.kzycAfter,
       })
+    },
+    onOk() {
+      const _bjl = (Number(this.data.bjlAfter) / 100 + 1) / (Number(this.data.bjl) / 100 + 1) - 1
+      const _bjsh = (Number(this.data.bjshAfter) / 100 + 1) / (Number(this.data.bjsh) / 100 + 1) - 1
+      const _sq = (Number(this.data.sqAfter) * 0.4555 / 100 + 1) / (Number(this.data.sq) * 0.4555 / 100 + 1) - 1
+      const _shts = (Number(this.data.shtsAfter) / 100 + 1) / (Number(this.data.shts) / 100 + 1) -1
+      const _jnsh = (Number(this.data.jnshAfter) / 100 + 1) / (Number(this.data.jnsh) / 100 + 1) -1
+      const _boss = (Number(this.data.bossAfter) / 100 + 1) / (Number(this.data.boss) / 100 + 1) -1
+      const _kzyc = (Number(this.data.kzycAfter) / 100 + 1) / (Number(this.data.kzyc) / 100 + 1) -1
+
+      const res = ((_bjl + _bjsh + _sq + _shts + _jnsh + _boss + _kzyc) * 100).toFixed(2)
+      this.setData({res: res})
+
+      wx.setStorageSync('bjl', this.data.bjl)
+      wx.setStorageSync('bjsh', this.data.bjsh)
+      wx.setStorageSync('sq', this.data.sq)
+      wx.setStorageSync('shts', this.data.shts)
+      wx.setStorageSync('jnsh', this.data.jnsh)
+      wx.setStorageSync('boss', this.data.boss)
+      wx.setStorageSync('kzyc', this.data.kzyc)
     },
     onInputChange(e: any) {
-      const nickName = e.detail.value
-      const { avatarUrl } = this.data.userInfo
+      const key = e.target.id;
       this.setData({
-        "userInfo.nickName": nickName,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    getUserProfile() {
-      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-      wx.getUserProfile({
-        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: (res) => {
-          console.log(res)
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+        [key]: e.detail.value
       })
     },
   },
